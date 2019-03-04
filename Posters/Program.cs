a44552889d5981e1df6a919d6e0527177f352128
kaajoj <kaajooj@gmail.com>
2019-03-04 22:02:37 +0100

    Posters v1.0

--- Posters/Program.cs
+++ Posters/Program.cs
@@ -1,21 +1,70 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Posters
 {
     class Program
     {
+        public static int buildingsCount;
+        public static string line;
+        public static int[,] bxy;
+        public static int posterCount;
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.WriteLine("POSTERS");
+            buildingsCount = Convert.ToInt32(Console.ReadLine());
+            if (buildingsCount > 0)
+            {
+                posterCount = 1;
+                bxy = new int[buildingsCount, 2];
+            }
+            else posterCount = 0;
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            for (int i = 0; i < buildingsCount; i++)
+            {
+                try
+                {
+                    readBuidlingDesc(i);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Wrong input - type correct building description(Two digits separated by space)");
+                    readBuidlingDesc(i);
+                }
+            }
+
+            int poster = calculatePosters(posterCount, buildingsCount, bxy);
+            Console.WriteLine(poster);
+
+            Console.ReadKey(true);
+        }
+
+        public static void readBuidlingDesc(int i)
+        {
+            line = Console.ReadLine();
+            string[] xy = line.Split(new char[] { ' ' });
+            bxy[i, 0] = Convert.ToInt32(xy[0]);
+            bxy[i, 1] = Convert.ToInt32(xy[1]);
+        }
+
+        public static int calculatePosters(int posterCount, int buildingsCount, int[,] cxy)
+        {
+            int poster = posterCount;
+            int minTemp = cxy[0, 1];
+
+            for (int x = 0; x < buildingsCount - 1; x++)
+            {
+                if (cxy[x + 1, 1] < minTemp)
+                {
+                    minTemp = cxy[x + 1, 1];
+                    poster++;
+                }
+                if (cxy[x + 1, 1] > cxy[x, 1]) poster++;
+                else if (cxy[x + 1, 1] < cxy[x, 1] && (cxy[x + 1, 1] > minTemp)) poster++;
+            }
+
+            return poster;
         }
     }
+
 }
